0af62dcf36737a8f29319d77a72d19f63b0b494a
Tino-tee <chitimatinotenda55@gmail.com>
2026-05-31 17:13:09 +0200

    Add project files.

---
+++ Leave_Management_System/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Leave_Management_System.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var data = new Models.TestViewModel
+            {
+                Name = "Tinotenda",
+                DateOfBirth = new DateTime(1990, 1, 1)
+
+            };
+            return View(data);
+        }
+    }
+}
---
+++ Leave_Management_System/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Leave_Management_System.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
